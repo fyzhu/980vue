7e142684cef23a3218c94d1e959b20a26267f13c
Zhuyunfeng <i@zhuyunfeng.com>
2018-05-03 15:03:32 +0800

    use vue-cli

---
+++ todolist/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div>
+      <input type="text" v-model="inputValue">
+      <button type="submit" @click="handleSubmit">提交</button>
+    </div>
+    <ul>
+      <todo-item
+        v-for="(item,index) of list"
+        :content="item"
+        :key="index"
+        :index="index"
+        @delete="handleDelete"
+      ></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  },
+  components: {
+    TodoItem
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ todolist/src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['content', 'index'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleDelete () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
